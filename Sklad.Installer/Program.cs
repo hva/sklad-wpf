f3db9e662e20badb7b0d859ad6ed5c7af041cfc5
Yauheni Khvaliuk <hvaluk@gmail.com>
2016-10-30 17:11:31 +0100

    add dlls

--- Sklad.Installer/Program.cs
+++ Sklad.Installer/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using WixSharp;
 
 namespace Sklad.Installer
@@ -7,13 +8,33 @@ namespace Sklad.Installer
     {
         static void Main(string[] args)
         {
-            var project = new Project("MyProduct",
-                                      new Dir(@"%ProgramFiles%\My Company\My Product",
-                                          new File("packages.config")));
+            var configuration = Environment.GetEnvironmentVariable("CONFIGURATION") ?? "Debug";
+            var project = new Project
+            {
+                Name = "Sklad Desktop",
+                GUID = new Guid("1EDF5D89-5BB2-4A12-B8FD-9DAA9F363639"),
+                UpgradeCode = new Guid("E1E7E6A8-D96D-4D26-926E-8F638B9A3992"),
+                Language = "ru-RU",
+                SourceBaseDir = @"..\Warehouse.Wpf\bin\" + configuration,
+                Dirs = new[]
+                {
+                    new Dir(@"%ProgramFiles%\Sklad",
+                        new DirFiles("*.dll")
+                    )
+                },
+                OutFileName = "Sklad",
+                PreserveTempFiles = true,
+                ControlPanelInfo =
+                {
+                    Manufacturer = "Skill",
+                    ProductIcon = @"..\Warehouse.Wpf\Icon.ico",
+                },
+                
+            };
 
-            project.GUID = new Guid("6f330b47-2577-43ad-9095-1861ba25889b");
+            project.ResolveWildCards();
 
-            Compiler.BuildMsi(project);
+            project.BuildMsi();
         }
     }
 }
