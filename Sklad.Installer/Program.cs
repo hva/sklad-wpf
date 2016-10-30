37ccb0988573b2618c4c849cdce0e7ae1d9ba590
Yauheni Khvaliuk <hvaluk@gmail.com>
2016-10-30 11:25:33 +0100

    build msi

---
+++ Sklad.Installer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using WixSharp;
+
+namespace Sklad.Installer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var project = new Project("MyProduct",
+                                      new Dir(@"%ProgramFiles%\My Company\My Product",
+                                          new File("packages.config")));
+
+            project.GUID = new Guid("6f330b47-2577-43ad-9095-1861ba25889b");
+
+            Compiler.BuildMsi(project);
+        }
+    }
+}
