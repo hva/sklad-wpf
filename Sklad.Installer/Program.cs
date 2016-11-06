4a10a08c733ec61daa6823dacef84562f4077b5d
Yauheni Khvaliuk <hvaluk@gmail.com>
2016-11-06 19:22:22 +0100

    update app config

--- Sklad.Installer/Program.cs
+++ Sklad.Installer/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
 using WixSharp;
 using WixSharp.CommonTasks;
 using WixSharp.Controls;
@@ -12,7 +15,7 @@ namespace Sklad.Installer
         static void Main(string[] args)
         {
             var configuration = Environment.GetEnvironmentVariable("CONFIGURATION") ?? "Debug";
-            var version = Environment.GetEnvironmentVariable("APPVEYOR_BUILD_VERSION") ?? "0.0.1.0";
+            var version = Environment.GetEnvironmentVariable("APPVEYOR_BUILD_VERSION") ?? "0.0.1";
             var project = new Project
             {
                 Version = new Version(version),
@@ -46,14 +49,38 @@ namespace Sklad.Installer
                     //AllowSameVersionUpgrades = true, //uncomment this if the the upgrade version is different by only the fourth field
                     Schedule = UpgradeSchedule.afterInstallInitialize,
                     DowngradeErrorMessage = "Более новая версия [ProductName] уже установлена."
-                }
+                },
+                Encoding = Encoding.UTF8,
+                LocalizationFile = Path.Combine(Environment.CurrentDirectory, "ru-ru.wxl"),
             };
 
             project.RemoveDialogsBetween(NativeDialogs.WelcomeDlg, NativeDialogs.InstallDirDlg);
 
             project.SetNetFxPrerequisite("NETFRAMEWORK45", "Пожалуйста установите\nMicrosoft .NET Framework 4.5");
 
+            project.IncludeWixExtension(WixExtension.Util);
+            project.WixSourceGenerated += Compiler_WixSourceGenerated;
+
             project.BuildMsi();
+        }
+
+        static void Compiler_WixSourceGenerated(XDocument document)
+        {
+            var file = document.FindAll("File").FirstOrDefault(
+                x => x.HasAttribute("Id", value => value.EndsWith("Warehouse.Wpf.exe.config"))
+            );
+
+            var baseAddress = Environment.GetEnvironmentVariable("BaseAddress") ?? "http://localhost:63270";
+
+            file?.Parent?.Add(
+                new XElement(WixExtension.Util.ToXNamespace() + "XmlFile",
+                    new XAttribute("Id", (string)file.Attribute("Id") + ".XmlFile"),
+                    new XAttribute("File", "[#" + (string)file.Attribute("Id") + "]"),
+                    new XAttribute("Action", "setValue"),
+                    new XAttribute("ElementPath", @"//appSettings/add[\[]@key='BaseAddress'[\]]/@value"),
+                    new XAttribute("Value", baseAddress)
+                )
+            );
         }
     }
 }
